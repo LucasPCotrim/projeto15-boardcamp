22930f1a2259b1a6cb244c4da0a6227c802e2c9e
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-29 03:44:22 -0300

    feat: Implemented categoryMiddleware

--- src/middleware/categoryMiddleware.js
+++ src/middleware/categoryMiddleware.js
@@ -1,5 +1,10 @@
 import categorySchema from '../schemas/categorySchema';
 
 export default function validateCategory(req, res, next) {
+  const { name } = req.body;
+  const { error: validError } = categorySchema.validate({ name });
+  if (validError) {
+    return res.status(400).send({ message: String(validError) });
+  }
   next();
 }
