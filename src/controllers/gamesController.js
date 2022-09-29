c3bbe875cb894be935d56af066491be1a5198f88
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-29 05:16:29 -0300

    feat: Implemented route GET '/games'

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,1 +1,32 @@
 import dbConnection from '../database/pgsql.js';
+
+async function getGames(req, res) {
+  // Obtain optional query params
+  const { name } = req.query;
+
+  try {
+    if (name) {
+      // Obtain games from Database (filter by name)
+      const { rows: games } = await dbConnection.query(
+        `SELECT games.*, categories.name AS "categoryName"
+        FROM games JOIN categories ON categories.id=games."categoryId"
+        WHERE games.name ILIKE $1`,
+        [`${name}%`]
+      );
+      res.status(200).send(games);
+    } else {
+      // Obtain games from Database
+      const { rows: games } = await dbConnection.query(
+        `SELECT games.*, categories.name AS "categoryName"
+        FROM games JOIN categories ON categories.id=games."categoryId"`
+      );
+      res.status(200).send(games);
+    }
+    // Error when fetching games from Database
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: 'An error occured when fetching games from Database' });
+  }
+}
+
+export { getGames };
