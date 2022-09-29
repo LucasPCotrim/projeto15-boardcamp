43df727ea2f16002fe55db098636f76d37ed86ce
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-29 17:46:41 -0300

    feat: Implemented customersMiddleware

--- src/middlewares/cutomersMiddleware.js
+++ src/middlewares/cutomersMiddleware.js
@@ -1,5 +1,19 @@
 import customerSchema from '../schemas/customerSchema.js';
 
 export default function validateCustomer(req, res, next) {
+  // Obtain customer from body
+  const { name, phone, cpf, birthday } = req.body;
+  // Validate customer
+  const { error: validError } = customerSchema.validate({
+    name,
+    phone,
+    cpf,
+    birthday,
+  });
+  if (validError) {
+    return res.status(400).send({ message: String(validError) });
+  }
+  // Store customer in res.locals
+  res.locals.customer = { name, phone, cpf, birthday };
   next();
 }
