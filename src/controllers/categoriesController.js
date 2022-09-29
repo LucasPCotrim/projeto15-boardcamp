199a46225832185ef997c39239fccff5aed271bf
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-29 03:57:15 -0300

    feat: Implemented route GET '/categories'

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,1 +1,15 @@
 import dbConnection from '../database/pgsql.js';
+
+async function getCategories(req, res) {
+  try {
+    const { rows: categories } = await dbConnection.query('SELECT * FROM categories');
+    return res.status(200).send(categories);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .send({ message: 'An error occured when fetching categories from Database' });
+  }
+}
+
+export { getCategories };
