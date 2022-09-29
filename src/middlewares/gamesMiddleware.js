630fbef941854378cad95d74c9f8a6164ac4ea4d
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-29 04:51:47 -0300

    feat: Implemented gamesMiddleware

--- src/middlewares/gamesMiddleware.js
+++ src/middlewares/gamesMiddleware.js
@@ -1,5 +1,20 @@
 import gameSchema from '../schemas/gameSchema.js';
 
 export default function validateGame(req, res, next) {
+  // Obtain game from body
+  const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+  // Validate game
+  const { error: validError } = gameSchema.validate({
+    name,
+    image,
+    stockTotal,
+    categoryId,
+    pricePerDay,
+  });
+  if (validError) {
+    return res.status(400).send({ message: String(validError) });
+  }
+  // Store game in res.locals
+  res.locals.game = { name, image, stockTotal, categoryId, pricePerDay };
   next();
 }
