fe420959a556b2736a7a91e84df72737630396b4
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-29 17:32:56 -0300

    feat: Implemented route GET '/customers'

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -1,1 +1,29 @@
 import dbConnection from '../database/pgsql.js';
+
+async function getCustomers(req, res) {
+  // Obtain optional query params
+  const { cpf } = req.query;
+
+  if (cpf) {
+    // Obtain customers from Database (filter by cpf)
+    const { rows: customers } = await dbConnection.query(
+      `SELECT * FROM customers
+      WHERE cpf LIKE $1;`,
+      [`${cpf}%`]
+    );
+    res.status(200).send(customers);
+  } else {
+    // Obtain customers from Database
+    const { rows: customers } = await dbConnection.query(`SELECT * FROM customers`);
+    res.status(200).send(customers);
+  }
+
+  try {
+    // Error when fetching customers from Database
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: 'An error occured when fetching games from Database' });
+  }
+}
+
+export { getCustomers };
--- src/routes/customersRouter.js
+++ src/routes/customersRouter.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { getCustomers } from '../controllers/customersController.js';
 
 const customersRouter = Router();
+
+customersRouter.get('/customers', getCustomers);
 
 export default customersRouter;
