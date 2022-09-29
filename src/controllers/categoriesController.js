dc0a0c5b1fbf332feadde21adee1fb7c86ad1664
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-29 04:43:37 -0300

    refactor: Added comments to routes GET /categories and POST /categories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -2,8 +2,11 @@ import dbConnection from '../database/pgsql.js';
 
 async function getCategories(req, res) {
   try {
+    // Obtain categories from Database
     const { rows: categories } = await dbConnection.query('SELECT * FROM categories');
     return res.status(200).send(categories);
+
+    // Error when fetching categories from Database
   } catch (error) {
     console.log(error);
     return res
@@ -14,19 +17,25 @@ async function getCategories(req, res) {
 
 async function createCategory(req, res) {
   try {
+    // Get category name from locals after middleware validation
     const { name } = res.locals.category;
+    // Check Database for existing category with the same name
     const checkExistingCategory = await dbConnection.query(
       'SELECT * FROM categories WHERE name = $1',
       [name]
     );
     if (checkExistingCategory.rowCount > 0) {
+      // Error: Existing category with the same name
       return res
         .status(409)
         .send({ message: `Error: Found an existing category with the name: ${name}` });
     }
 
+    // Insert category into Database
     await dbConnection.query('INSERT INTO categories (name) VALUES ($1)', [name]);
     res.sendStatus(201);
+
+    // Error when inserting category into Database
   } catch (error) {
     console.log(error);
     return res
--- src/middlewares/categoriesMiddleware.js
+++ src/middlewares/categoriesMiddleware.js
@@ -1,11 +1,14 @@
 import categorySchema from '../schemas/categorySchema.js';
 
 export default function validateCategory(req, res, next) {
+  // Obtain category name from body
   const { name } = req.body;
+  // Validate category
   const { error: validError } = categorySchema.validate({ name });
   if (validError) {
     return res.status(400).send({ message: String(validError) });
   }
+  // Store category in res.locals
   res.locals.category = { name };
   next();
 }
