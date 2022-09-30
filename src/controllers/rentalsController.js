f16463511c10df8208c6dd19681d26fdd4d8063f
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-29 22:37:49 -0300

    feat: Implemented route POST '/rentals'

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -25,7 +25,7 @@ async function getRentals(req, res) {
       `SELECT
         rentals.*,
         json_build_object('id', customers.id, 'name', customers.name) AS customer,
-        json_build_object('id', games.id, 'name', games.name, 'categoryId', games."categoryId", 'categoryName', categories.name) AS game,
+        json_build_object('id', games.id, 'name', games.name, 'categoryId', games."categoryId", 'categoryName', categories.name) AS game
       FROM rentals
         JOIN customers ON customers.id = rentals."customerId"
         JOIN games ON games.id = rentals."gameId"
@@ -45,6 +45,60 @@ async function getRentals(req, res) {
   }
 }
 
-async function createRental(req, res) {}
+async function createRental(req, res) {
+  // Get rental info from locals after middleware validation
+  const { customerId, gameId, daysRented } = res.locals.rental;
+
+  try {
+    // Check if customerId is valid
+    const checkCustomer = await dbConnection.query(
+      `SELECT id FROM customers 
+      WHERE id = $1`,
+      [customerId]
+    );
+    if (checkCustomer.rowCount === 0) {
+      return res.status(400).send({ message: 'Error: Invalid customerId' });
+    }
+
+    // Check if gameId is valid
+    const checkGame = await dbConnection.query(
+      `SELECT * FROM games 
+      WHERE id = $1`,
+      [gameId]
+    );
+    if (checkGame.rowCount === 0) {
+      return res.status(400).send({ message: 'Error: Invalid gameId' });
+    }
+    const game = checkGame.rows[0];
+
+    // Check if gameId is available
+    const checkRentals = await dbConnection.query(
+      `SELECT id
+      FROM rentals 
+      WHERE "gameId" = $1 AND "returnDate" IS null`,
+      [gameId]
+    );
+    if (checkRentals.rowCount > 0 && game.stockTotal === checkRentals.rowCount) {
+      return res.status(400).send({ message: 'Error: Game not available' });
+    }
+
+    // Insert rental into Database
+    const originalPrice = daysRented * game.pricePerDay;
+    await dbConnection.query(
+      `INSERT INTO rentals
+      ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
+      VALUES ($1, $2, NOW(), $3, null, $4, null)`,
+      [customerId, gameId, daysRented, originalPrice]
+    );
+    res.sendStatus(201);
+
+    // Error when inserting rental into Database
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .send({ message: 'An error occured when inserting rental into Database' });
+  }
+}
 
 export { getRentals, createRental };
